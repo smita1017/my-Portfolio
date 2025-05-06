6b515a2fedff430fc65837e7f84122fc923022a9
smita1017 <smita126@outlook.com>
2025-05-06 11:43:01 +0530

    new commit

--- src/Pages/Handledownloadcv.js
+++ src/Pages/Handledownloadcv.js
@@ -2,7 +2,7 @@ import React from 'react'
 const handleDownloadCV = () => {
   if (window.confirm('Do you want to download the resume?')) {
     const link = document.createElement('a');
-    link.href = '/my-Portfolio/Smita_Patil_resume.pdf';
+    link.href = import.meta.env.BASE_URL + 'Smita_Patil_resume.pdf';
     link.download = 'Smita_Patil_Resume.pdf';
     document.body.appendChild(link);
     link.click();
