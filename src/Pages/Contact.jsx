2d818d96d21d6bbd4e371dab3630ccd3b850740f
smita1017 <smita126@outlook.com>
2025-05-07 15:45:10 +0530

    updated url

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch("https://backendportfolio-tkpx.onrender.com", {
+      const response = await fetch("https://backendportfolio-tkpx.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
